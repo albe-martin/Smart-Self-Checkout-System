ed4e746241eae7ef97f750f935b802792d4ce831
Braedon <braedon.haensel@ucalgary.ca>
2023-04-05 17:06:36 -0600

    Implemented adding languages to Language class.

--- com.autovend.software/src/com/autovend/software/swing/Language.java
+++ com.autovend.software/src/com/autovend/software/swing/Language.java
@@ -1,9 +1,15 @@
 package com.autovend.software.swing;
 
+import java.util.HashMap;
+
 /**
  * Class used for translating text to the customer's desired language.
  */
 public class Language {
+	/**
+	 * HashMap containing each language and the direct text translations of that language.
+	 */
+	private static HashMap<String, HashMap<String, String>> translator = new HashMap<>();
 	/**
 	 * Translates the text to the desired language.
 	 * 
@@ -16,9 +22,34 @@ public class Language {
 		if (language.equalsIgnoreCase("English")) {
 			// If language is English, translation is not required.
 			return text;
+			
+		} else if (translator.containsKey(language.toLowerCase())) {
+			// Translate to language.
+			String translation = translator.get(language.toLowerCase()).get(text);
+			
+			if (translation == null) {
+				// Translation not found, return original english text.
+				return text;
+			} else {
+				// Return translation.
+				return translation;
+			}
+			
 		} else {
 			// Language not supported.
 			throw new IllegalArgumentException("Language not supported.");
 		}
+	}
+	
+	/**
+	 * Add a new language to the translator HashMap.
+	 * 
+	 * @param language
+	 * 			Name of the new language.
+	 * @param translations
+	 * 			HashMap of direct translations from English to the given language.
+	 */
+	public static void addLanguage(String language, HashMap<String, String> translations) {
+		translator.put(language.toLowerCase(), translations);
 	}
 }
