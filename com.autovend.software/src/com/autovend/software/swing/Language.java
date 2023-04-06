bac4fcb22d7a772071a12bcf9b7059ce3221dbf7
ColtG5 <colton.gowans@ucalgary.ca>
2023-04-05 20:18:29 -0600

    Commented the code

--- com.autovend.software/src/com/autovend/software/swing/Language.java
+++ com.autovend.software/src/com/autovend/software/swing/Language.java
@@ -3,9 +3,19 @@ package com.autovend.software.swing;
 import java.util.HashMap;
 
 /**
- * Class used for translating text to the customer's desired language.
+ * Class used for translating the programmers (english) text to the customer's desired language.
  */
 public class Language {
+	// Map looks like {
+	//   	programmer's text: {
+	//   		language: that language's text,
+	//   		language2: another language's text
+	// 		}
+	// 	 	another text for another component: {
+	// 	 		language: that language's text,
+	// 	 		language2: another language's text
+	// 		}
+	// 	 }
 	public static HashMap<String, HashMap<String, String>> languageBank = new HashMap<>() {{
 		put("Log In", new HashMap<>(){{
 			put("English", "Log In");
@@ -25,19 +35,12 @@ public class Language {
 	}};
 
 	/**
-	 * Translates the text to the desired language.
-	 *
-	 * @param text
-	 * 			The text to be translated. Must be provided in English.
+	 * Translates the english text for a gui element into the language specified
+	 * @param language the language used for an instance of the calling gui
+	 * @param text the text to be translated
+	 * @return the translated text
 	 */
 	public static String translate(String language, String text) {
-//		if (selfCheckoutLanguage.equalsIgnoreCase("English")) {
-//			// If language is English, translation is not required.
-//			return text;
-//		} else {
-//			// Language not supported.
-//			throw new IllegalArgumentException("Language not supported.");
-//		}
 		return languageBank.get(text).get(language);
 	}
 }
