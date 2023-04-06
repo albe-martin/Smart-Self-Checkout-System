c1a0aef7587aaebb3e92f66b61a949253ea223aa
ColtG5 <colton.gowans@ucalgary.ca>
2023-04-05 20:08:31 -0600

    Updated logic for translating text

--- com.autovend.software/src/com/autovend/software/swing/Language.java
+++ com.autovend.software/src/com/autovend/software/swing/Language.java
@@ -1,24 +1,43 @@
 package com.autovend.software.swing;
 
+import java.util.HashMap;
+
 /**
  * Class used for translating text to the customer's desired language.
  */
 public class Language {
+	public static HashMap<String, HashMap<String, String>> languageBank = new HashMap<>() {{
+		put("Log In", new HashMap<>(){{
+			put("English", "Log In");
+		}});
+		put("Log Out", new HashMap<>(){{
+			put("English", "Log Out");
+		}});
+		put("Username:", new HashMap<>(){{
+			put("English", "Username:");
+		}});
+		put("Password:", new HashMap<>(){{
+			put("English", "Password:");
+		}});
+		put("Exit", new HashMap<>(){{
+			put("English", "Exit");
+		}});
+	}};
+
 	/**
 	 * Translates the text to the desired language.
-	 * 
-	 * @param language
-	 * 			The language to translate to.
+	 *
 	 * @param text
 	 * 			The text to be translated. Must be provided in English.
 	 */
 	public static String translate(String language, String text) {
-		if (language.equalsIgnoreCase("English")) {
-			// If language is English, translation is not required.
-			return text;
-		} else {
-			// Language not supported.
-			throw new IllegalArgumentException("Language not supported.");
-		}
+//		if (selfCheckoutLanguage.equalsIgnoreCase("English")) {
+//			// If language is English, translation is not required.
+//			return text;
+//		} else {
+//			// Language not supported.
+//			throw new IllegalArgumentException("Language not supported.");
+//		}
+		return languageBank.get(text).get(language);
 	}
 }
