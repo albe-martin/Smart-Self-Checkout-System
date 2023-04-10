9ffe1e3e306e36c5a3d692fa23495c89b1e7be78
Fatema-Ch <115423993+Fatema-Ch@users.noreply.github.com>
2023-04-10 15:05:19 -0600

    Tests for membership

--- com.autovend.software.test/src/com/autovend/software/test/MembershipTest.java
+++ com.autovend.software.test/src/com/autovend/software/test/MembershipTest.java
@@ -2,16 +2,15 @@ package com.autovend.software.test;
 
 import static org.junit.Assert.*;
 
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import com.autovend.Numeral;
 import com.autovend.devices.BarcodeScanner;
 import com.autovend.software.controllers.BarcodeScannerController;
 import com.autovend.software.controllers.CheckoutController;
 import com.autovend.software.controllers.CustomerIOController;
-import com.autovend.software.utils.BarcodeUtils;
 import com.autovend.software.utils.MembershipDatabases;
 import com.autovend.software.controllers.CardReaderController;
 import com.autovend.devices.CardReader;
@@ -39,19 +38,16 @@ public class MembershipTest {
     	stubDevice = new TouchScreen();
         checkoutController = new CheckoutController();
         cardReaderController = new CardReaderController(stubCardReader);
-        //cardReaderController.setMainController(checkoutController);
-        //checkoutController.registerController("ValidPaymentControllers", cardReaderController);
-        checkoutController.registerController("CardReaderController", cardReaderController);
+        cardReaderController.setMainController(checkoutController);
+        checkoutController.registerController("ValidPaymentControllers", cardReaderController);
         
         barcodeScannerController = new BarcodeScannerController(stubScanner);
-        //barcodeScannerController.setMainController(checkoutController);
-        //checkoutController.registerController("ItemAdderController", barcodeScannerController);
-        checkoutController.registerController("BarcodeScannerController", barcodeScannerController);
+        barcodeScannerController.setMainController(checkoutController);
+        checkoutController.registerController("ItemAdderController", barcodeScannerController);
     
 		customerController = new CustomerIOController(stubDevice);
 		customerController.setMainController(checkoutController);
 		checkoutController.registerController("CustomerIOController", customerController);
-		//
     }
 	
 	@After
@@ -73,35 +69,33 @@ public class MembershipTest {
 		
 	}
 	
-//	@Test
-//	public void TestvalidateMembership() {
-//		membershipID = "123456789";
-//		Numeral[] ID = BarcodeUtils.stringToNumeralArray(membershipID);
-//		MembershipDatabases.MEMBERSHIP_DATABASE.put(ID, membershipID);
-//		checkoutController.validateMembership(membership);
-//	    assertNotNull(cardReaderController.state);
-//		assertTrue(barcodeScannerController.getScanningItems());
-//
-//	}
-//	
-	
-//	@Test
-//	public void TestCancelSignIn() {
-//		checkoutController.cancelSigningInAsMember();
-//		assertTrue(barcodeScannerController.getScanningItems());
-//		assertNotNull(cardReaderController.state);
-//	}
-//	
-//	@Test
-//	public void TestInValidMembership() {
-//		membershipID = "123456789";
-//		Numeral[] ID = BarcodeUtils.stringToNumeralArray(membershipID);
-//		checkoutController.signingInAsMember();
-//		checkoutController.validateMembership(membership);
-//		assertFalse(barcodeScannerController.getScanningItems());
-//		assertNotNull(cardReaderController.state);
+	@Test
+	public void TestvalidateMembership() {
+		membershipID = "123456789";
+		MembershipDatabases.MEMBERSHIP_DATABASE.put(membershipID, "some member");
+		checkoutController.validateMembership(membershipID);
+	    assertNotNull(cardReaderController.state);
+		assertTrue(barcodeScannerController.getScanningItems());
 
-//
-//	}
+	}
+	
+	
+	@Test
+	public void TestCancelSignIn() {
+		checkoutController.cancelSigningInAsMember();
+		assertTrue(barcodeScannerController.getScanningItems());
+		assertNotNull(cardReaderController.state);
+	}
+	
+	@Test
+	public void TestInValidMembership() {
+		membershipID = "999999999";
+		checkoutController.signingInAsMember();
+		checkoutController.validateMembership(membershipID);
+		assertFalse(barcodeScannerController.getScanningItems());
+		assertNotNull(cardReaderController.state);
+
+
+	}
 
 }
