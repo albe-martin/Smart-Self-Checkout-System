a0b504f04777f38643e6b74a8739e83031845f46
samanthaaliu <samantha.liu10@gmail.com>
2023-04-09 18:04:01 -0600

    added update printer test

--- com.autovend.software.test/src/com/autovend/software/test/StubBarcodedProduct.java
+++ com.autovend.software.test/src/com/autovend/software/test/StubBarcodedProduct.java
@@ -7,50 +7,50 @@ import com.autovend.devices.SimulationException;
 import com.autovend.products.Product;
 
 public class StubBarcodedProduct extends Product{
-	private Barcode barcode;
-	private String description;
-	private double expectedWeight;
+    private Barcode barcode;
+    private String description;
+    private double expectedWeight;
 
-	public StubBarcodedProduct(Barcode barcode, String description, BigDecimal price, double expectedWeight, boolean isPerUnit) {
-		super(price, isPerUnit);
+    public StubBarcodedProduct(Barcode barcode, String description, BigDecimal price, double expectedWeight, boolean isPerUnit) {
+        super(price, isPerUnit);
 
-		if(barcode == null)
-			throw new SimulationException(new NullPointerException("barcode is null"));
+        if(barcode == null)
+            throw new SimulationException(new NullPointerException("barcode is null"));
 
-		if(description == null)
-			throw new SimulationException(new NullPointerException("description is null"));
+        if(description == null)
+            throw new SimulationException(new NullPointerException("description is null"));
 
-		this.barcode = barcode;
-		this.description = description;
-		this.expectedWeight = expectedWeight;
-	}
+        this.barcode = barcode;
+        this.description = description;
+        this.expectedWeight = expectedWeight;
+    }
 
-	/**
-	 * Get the barcode.
-	 * 
-	 * @return The barcode. Cannot be null.
-	 */
-	public Barcode getBarcode() {
-		return barcode;
-	}
+    /**
+     * Get the barcode.
+     * 
+     * @return The barcode. Cannot be null.
+     */
+    public Barcode getBarcode() {
+        return barcode;
+    }
 
-	/**
-	 * Get the description.
-	 * 
-	 * @return The description. Cannot be null.
-	 */
-	public String getDescription() {
-		return description;
-	}
+    /**
+     * Get the description.
+     * 
+     * @return The description. Cannot be null.
+     */
+    public String getDescription() {
+        return description;
+    }
 
-	/**
-	 * Get the expected weight of one unit of the product.
-	 * 
-	 * @return The expected weight of one unit of the product.
-	 */
-	public double getExpectedWeight() {
-		return expectedWeight;
-	}
-	
+    /**
+     * Get the expected weight of one unit of the product.
+     * 
+     * @return The expected weight of one unit of the product.
+     */
+    public double getExpectedWeight() {
+        return expectedWeight;
+    }
+    
 
 }
