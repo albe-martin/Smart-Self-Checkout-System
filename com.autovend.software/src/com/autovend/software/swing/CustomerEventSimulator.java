e64142ec1626daf0fca0ba70923fc29d348114d2
hamza-khan3 <127697674+hamza-khan3@users.noreply.github.com>
2023-04-10 22:56:30 -0600

    finishing up customer event simulator

--- com.autovend.software/src/com/autovend/software/swing/CustomerEventSimulator.java
+++ com.autovend.software/src/com/autovend/software/swing/CustomerEventSimulator.java
@@ -43,24 +43,56 @@ public class CustomerEventSimulator extends JFrame {
                 
            }
         });
-        GridBagConstraints gbcBag1 = new GridBagConstraints();
-        gbcBag1.fill = GridBagConstraints.BOTH;
-        gbcBag1.gridx = 0;
-        gbcBag1.gridy = 0;
-        contentPane.add(scanItem, gbcBag1);
-//
-//
-//        // Low coins events
-//        JButton lowChange1 = new JButton("Create Low Coin Notification (1)");
-//        lowChange1.addActionListener(new ActionListener() {
-//            public void actionPerformed(ActionEvent e) {
-//                ((AttendantOperationPane) attendantFrame.getContentPane()).notifyLowCoinDenomination(cioc1, new BigDecimal("0.25"));
-//            }
-//        });
-//        GridBagConstraints gbcChange1 = new GridBagConstraints();
-//        gbcChange1.fill = GridBagConstraints.BOTH;
-//        gbcChange1.gridx = 0;
-//        gbcChange1.gridy = 1;
-//        contentPane.add(lowChange1, gbcChange1);
+        GridBagConstraints gbcScan = new GridBagConstraints();
+        gbcScan.fill = GridBagConstraints.BOTH;
+        gbcScan.gridx = 0;
+        gbcScan.gridy = 0;
+        contentPane.add(scanItem, gbcScan);
+
+
+       
+        JButton scanMembership = new JButton("Scan membership");
+        scanMembership.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                
+            }
+        });
+        GridBagConstraints gbcMembership = new GridBagConstraints();
+        gbcMembership.fill = GridBagConstraints.BOTH;
+        gbcMembership.gridx = 1;
+        gbcMembership.gridy = 0;
+        contentPane.add(scanMembership, gbcMembership);
+        
+        JButton doNotBag = new JButton("Do not bag item");
+        doNotBag.addActionListener(new ActionListener() {
+        public void actionPerformed(ActionEvent e) {
+            	
+                
+           }
+        });
+        GridBagConstraints gbcNoBag = new GridBagConstraints();
+        gbcNoBag.fill = GridBagConstraints.BOTH;
+        gbcNoBag.gridx = 0;
+        gbcNoBag.gridy = 1;
+        contentPane.add(doNotBag, gbcNoBag);
+        
+        
+        JButton addOwnBag = new JButton("Use own bag");
+        addOwnBag.addActionListener(new ActionListener() {
+        public void actionPerformed(ActionEvent e) {
+            	
+                
+           }
+        });
+        GridBagConstraints gbcAddOwnBag = new GridBagConstraints();
+        gbcAddOwnBag.fill = GridBagConstraints.BOTH;
+        gbcAddOwnBag.gridx = 1;
+        gbcAddOwnBag.gridy = 1;
+        contentPane.add(addOwnBag, gbcAddOwnBag);
+       
+            	
+                
+        
+        
     }
 }
